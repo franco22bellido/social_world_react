2b884a8762afc1811fe1a8454a71ca5e39a319df
franco22bellido <franco.bellido22@gmail.com>
2024-04-27 23:32:39 -0300

    follow and unfollow users

---
+++ src/API/followers.api.js
@@ -0,0 +1,51 @@
+import axios from "./axios.config"
+import {baseUrl} from './base.url'
+
+export const getFollowers = async ()=> {
+    try {
+        const res = axios.get(`${baseUrl}followers`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const deleteFollower =(followerId)=> {
+    try {
+        const res = axios.delete(`${baseUrl}followers/${followerId}`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const getFollowings = async ()=> {
+    try {
+        const res = await axios.get(`${baseUrl}following`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const deleteFollowing = async (followingId)=> {
+    try {
+        const res = await axios.delete(`${baseUrl}following/${followingId}`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const followUser = async (followingId)=> {
+    try {
+        const res = await axios.post(`${baseUrl}following/${followingId}`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const getFollowingsByFollowingId = async (followingId)=> {
+    try {
+        const res = await axios.get(`${baseUrl}following/${followingId}`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -3,15 +3,30 @@ import { getUserProfile } from "../../API/profile.api"
 import PostsList from "../../components/posts/PostsList"
 import { useParams } from "react-router-dom"
 import {useAuth} from '../../contexts/AuthContext'
+import {Link} from 'react-router-dom'
+import { getFollowingsByFollowingId , followUser, deleteFollowing } from "../../API/followers.api"
 
 const Profile = () => {
   const {username} = useParams()
   const {user} = useAuth()
   const [data, setData] = useState(false)
+  
 
   const getProfile = async ()=> {
     const {data} = await getUserProfile(user.token, username)
-    setData(data)
+    if(username !== user.user.username){
+      const {state}= await getFollowingsByFollowingId(data.id)
+      return setData({...data, state})
+    }
+    return setData(data)
+  }
+  const followOne = async (followingId)=> {
+    if(data.state){
+      await deleteFollowing(followingId)
+      return setData({...data, state: !data.state})
+    }
+    await followUser(followingId)
+    return setData({...data, state: !data.state})
   }
   useEffect(()=> {
     getProfile()
@@ -26,10 +41,14 @@ const Profile = () => {
           <div>
             <h1>{data.username}</h1>
             <h4>{data.profile.firstname} {data.profile.lastname}</h4>
-            <h4>followers {data.profile.followersCount}</h4>
-            <button>see followers</button>
-            <h4>following {data.profile.followingCount}</h4>
-            <button>see following</button>
+            <Link>followers: {data.profile.followersCount}</Link> <br />
+            <Link>followings: {data.profile.followingCount}</Link>
+
+            {
+              username !== user.user.username &&
+              <button onClick={()=>followOne(data.id)}>{data.state ? 'unfollow' : 'follow'}</button>
+            }
+
             <h4>created At : {data.profile.createdAt}</h4>
           </div>
 
