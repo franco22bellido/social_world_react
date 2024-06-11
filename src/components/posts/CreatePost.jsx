a663e945472a0aadec37a6e4e2d5acd7d1b18c5e
franco22bellido <franco.bellido22@gmail.com>
2024-06-11 03:14:43 -0300

    compress using Resizer from reac-image-file-resizer

--- src/components/posts/CreatePost.jsx
+++ src/components/posts/CreatePost.jsx
@@ -5,28 +5,43 @@ import Loader from '../Loader'
 import ErrorList from '../ErrorsList'
 import { useState } from 'react'
 import { uploadImage } from '../../API/photos.api'
+import Resizer from 'react-image-file-resizer'
 
 const CreatePost = ({ posts, setPosts }) => {
   const { handleSubmit, register, reset } = useForm()
   const { addPost, errors } = UsePosts()
   const [loading, setLoading] = useState(false)
-  const [photo ,setPhoto] = useState(null)
+  const [file, setFile] = useState(null)
   const { user } = useAuth()
 
   const onSubmit = async (values) => {
     reset()
     setLoading(true)
-    if(photo){
-      const formData = new FormData()
-      formData.append('photo', photo)
+    try {
+    if (file) {
+        //compress image
+        const uri = await new Promise((resolve) => { 
+          Resizer.imageFileResizer(file, 40000, 40000, 'JPEG', 30, 0, async (uri) => {
+            //return uri in base64
+            resolve(uri)
+          });
+        })
+        const photo = await fetch(uri).then((res) => res.blob());
+        const formData = new FormData()
+        formData.append('photo', photo)
+  
+        const res = await uploadImage(formData)
+        values = { ...values, imgUrl: res.data.url }
+      }
 
-      const res = await uploadImage(formData)
-      values = {...values, imgUrl: res.data.url}
-    }
-    const data = await addPost(values)
-    setLoading(false)
-    if(data){
-      setPosts([{ ...data, username: user.username }, ...posts])
+      const data = await addPost(values)
+      if (data) {
+        setPosts([{ ...data, username: user.username }, ...posts])
+        }
+      setLoading(false)
+    } catch (error) {
+        setLoading(false)
+        console.error('Error', error);
     }
   }
   return (
@@ -37,11 +52,11 @@ const CreatePost = ({ posts, setPosts }) => {
           e.target.style.height = `${e.target.scrollHeight}px`;
         }}
         className='form-area p-2' placeholder='write a new post' name="" id="" {...register("text", { required: true })}></textarea>
-      <input onChange={(e)=> {setPhoto(e.target.files[0])}} type="file"></input>
+      <input onChange={(e) => { setFile(e.target.files[0]) }} type="file"></input>
       <button className='btn-black mx-auto mt-1 mb-5 w-full md:w-1/4 md:mx-0' type='submit'>create post</button>
       <Loader loading={loading} />
-      <ErrorList errors={errors}/> 
-    
+      <ErrorList errors={errors} />
+
     </form>
   )
 }
